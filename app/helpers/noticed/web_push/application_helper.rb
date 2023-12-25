0ec1d6b19c4cf75231d62b3a32efee6c8535a205
Jonathan Bennett <jonathan@jbennett.me>
2023-12-25 16:39:41 -0500

    load helpers

--- app/helpers/noticed/web_push/application_helper.rb
+++ app/helpers/noticed/web_push/application_helper.rb
@@ -1,6 +1,10 @@
 module Noticed
   module WebPush
     module ApplicationHelper
+
+      def web_push_public_key_meta_tag(web_push_public_key: Rails.application.credentials.dig(:web_push, :public_key))
+        tag.meta name: "web_push_public", content: Base64.urlsafe_decode64(web_push_public_key).bytes.to_json
+      end
     end
   end
 end
--- lib/noticed/web_push/engine.rb
+++ lib/noticed/web_push/engine.rb
@@ -1,7 +1,14 @@
+# TODO why is this not autoloading
+require_relative "../../../app/helpers/noticed/web_push/application_helper"
+
 module Noticed
   module WebPush
     class Engine < ::Rails::Engine
       isolate_namespace Noticed::WebPush
+
+      ActiveSupport.on_load(:action_view) do
+        include Noticed::WebPush::ApplicationHelper
+      end
     end
   end
 end
