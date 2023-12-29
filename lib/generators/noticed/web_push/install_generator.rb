bf9cd027ee678c93171dcabae4fad39107e52ef4
Jonathan Bennett <jonathan@jbennett.me>
2023-12-29 09:44:50 -0500

    GENERATORz!

--- lib/generators/noticed/web_push/install_generator.rb
+++ lib/generators/noticed/web_push/install_generator.rb
@@ -6,8 +6,9 @@ module Noticed
 
         def add_javascript_deps
           run "yarn add github:jbennett/noticed-web_push"
-          inject_into_file File.join("app", "javascript", "application.js") do
+          append_to_file File.join("app", "javascript", "application.js") do
             <<~INIT
+
             import * as web_push from "noticed-web_push/web_push"
             web_push.start({
               mounted_path: "/web_push",
@@ -17,19 +18,41 @@ module Noticed
           end
         end
 
-        # - javascript
-        #   - add to package
-        #   - add to application.js
-        # - service worker
-        #   - add entry point file
-        #   - add to assests precompile
-        # - webmanifest
-        #   - mk asset dir
-        #   - make sample
-        # - ruby
-        #   - add subscriptions to user
-        #   - add routes helper
-        #   - add to layout (helper + webmanifest)
+        def setup_service_worker
+          run "touch app/javascript/service_worker.js"
+          append_to_file File.join("app", "javascript", "service_worker.js") do
+            <<~INIT
+            import {start} from "noticed-web_push/service_worker"
+            start({
+              mounted_path: "/web_push",
+            })
+            INIT
+          end
+          append_to_file "config/initializers/assets.rb", "Rails.application.config.assets.precompile += %w( service_worker.js )"
+        end
+
+        def create_webmanifest
+          template "app.webmanifest.tt", "app/assets/webmanifests/app.webmanifest"
+          append_to_file "app/assets/config/manifest.js", "//= link_tree ../webmanifests"
+        end
+
+        def update_rubies
+          inject_into_file "app/models/user.rb", after: "has_many :notifications, as: :recipient, dependent: :destroy\n" do
+            "\thas_many :web_push_subscriptions, class_name: \"Noticed::WebPush::Subscription\", dependent: :destroy\n"
+          end
+          route "mount_web_push \"/web_push\", :service_worker, :webmanifest\n"
+
+          inject_into_file "app/views/layouts/application.html.erb", before: "</head>" do
+            <<~INIT
+
+            \t\t<%= link_tag rel: :manifest, href: "/app.webmanifest" %>
+            \t\t<%= web_push_public_key_meta_tag %>
+            INIT
+          end
+          inject_into_file "app/views/layouts/application.html.erb", after: "<body>\n" do
+            "\t\t<button id=\"enable_notifications\" style=\"display: none\">Enable Notifications</button>\n"
+          end
+        end
       end
     end
   end
