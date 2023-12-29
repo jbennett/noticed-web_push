abbd334a8d4673bd6681232eeb485260531b9407
Jonathan Bennett <jonathan@jbennett.me>
2023-12-29 06:18:23 -0500

    update worker

--- app/assets/javascripts/noticed/web_push/service_worker.js
+++ app/assets/javascripts/noticed/web_push/service_worker.js
@@ -1,4 +1,5 @@
 ENGINE_MOUNT_PATH = null
+VERSION = 1
 
 export function start(options) {
   ENGINE_MOUNT_PATH = options.mounted_path
@@ -6,7 +7,7 @@ export function start(options) {
 
 self.addEventListener('push', event => {
     const data = event.data?.json() || {}
-    console.log("Received push", data)
+    console.log(`v${VERSION}: Received push`, data)
 
     event.waitUntil(
         self.registration.showNotification(data.title, {
